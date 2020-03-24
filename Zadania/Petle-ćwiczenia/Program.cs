e4b3f25f0c83d3c9a9a940d228248984cccd6a56
lewanita <anitamlew@gmail.com>
2020-03-24 15:03:08 +0100

    Praktyczne zastosowanie pętli

---
+++ Zadania/Petle-ćwiczenia/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+
+namespace Petle_ćwiczenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Wypisz liczby od 1 do 100
+            //for(int i = 1; i < 100; i++) 
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+            #endregion
+
+            #region Wypisz liczby parzyste od 2 do 50
+
+            //var czyParzysta = true;
+
+            //if(czyParzysta) 
+            //{
+            //    for (int i = 2; i < 50; i++)
+            //    {
+            //        if (i % 2 == 0 && i % 4 == 0) 
+            //        {
+            //            Console.WriteLine(i);
+            //            czyParzysta = false;
+            //        }
+
+            //    }   
+            //}
+            //Console.ReadLine();
+            #endregion
+
+            #region Wypisz liczby od 100 do 1
+
+            //for(int i = 100; i>=1;i--) 
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+
+            #region Wypisz liczby podzielne przez 8 w zakresie 1 do 100
+
+            //bool czyPodzielna = true;
+            //if (czyPodzielna) 
+            //{
+            //for(int i = 1; i < 100; i++) 
+            //    {
+            //        if (i % 8 == 0) 
+            //        {
+            //            Console.WriteLine(i);
+            //            czyPodzielna = false;
+            //        }
+
+            //    }
+            //    Console.ReadLine();
+            //}
+            #endregion
+
+            #region Wypisz najwieksza liczb niepodzielną przez 2,3,5,7, ale mniejsza od 1000
+
+            //for (int i = 1000; i >0; i--)
+            //{
+            //if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0)
+            //{
+            //    Console.WriteLine(i);
+            //    i = 0; // przeciążenie for lub break; 
+            //}  
+
+            //}
+
+
+
+            #endregion
+
+            #region Sprawdź czy podana jako parametr liczba jest parzysta. Istnieje mozliwosc wpisywania wartości do sprawdzenia wielokrotnie
+
+            //bool endLoop = true;
+            //while (endLoop) 
+            //{
+            //    Console.WriteLine(" Enter a number:");
+            //    var userNum = Console.ReadLine();
+
+            //    if (userNum=="q") 
+            //    {
+            //        endLoop = false;
+            //        continue;
+            //    }
+            //    int num2;
+            //    var result = int.TryParse(userNum, out num2);
+            //    if (num2 % 2 == 0) 
+            //    {
+            //        Console.WriteLine($"{userNum} jest liczba parzysta");
+            //    }
+            //  else 
+            //    {
+            //        Console.WriteLine($"{userNum} Nie jest liczba parzystą");
+            //    }
+            //    Console.ReadLine();
+
+
+
+            //}
+
+            #endregion
+
+            #region sprawdź czy podana jako parametr liczba jest liczbą pierwszą (podzielna tylko przez 1 i przez samą siebie). Mozesz wprowadzic tylko jedna liczbe do sprawdzenia
+
+            //Console.WriteLine("Enetr a number:");
+            //var userNum = Console.ReadLine();
+            //int checkingNum;
+            //var result = int.TryParse(userNum, out checkingNum);
+
+            //if (result)
+            //{
+            //    if (checkingNum % 2 == 0 && checkingNum % 3 == 0 && checkingNum % 5 == 0)
+            //    {
+            //        Console.WriteLine($"{checkingNum} nie jest liczba pierwsza");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("To jest liczba pierwsza");
+            //    }
+
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("Błędna wartość");
+            //}
+            //Console.ReadLine();
+            #endregion
+
+            #region  sprawdź czy podana jako parametr liczba jest liczbą pierwszą (podzielna tylko przez 1 i przez samą siebie). Mozesz wprowadzic wiele liczb do sprawdzenia
+
+            //bool czyPierwsza = true;
+            //while (czyPierwsza) 
+            //{
+            //    Console.WriteLine("Enter a number:");
+            //    var userNum = Console.ReadLine();
+            //    if (userNum == "q") 
+            //    {
+            //        czyPierwsza = false;
+            //        continue;
+            //    }
+
+            //    int checkingNum;
+            //    var result = int.TryParse(userNum, out checkingNum);
+
+            //    if (result) 
+            //    {
+            //    if(checkingNum%2==0 && checkingNum%3==0 && checkingNum % 5 == 0) 
+            //        {
+            //            Console.WriteLine($"{checkingNum} nie jest liczba pierwsza");
+
+            //        }
+            //    else 
+            //        {
+            //            Console.WriteLine($"{checkingNum} jest liczba pierwsza");
+            //        }
+
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine("Koniec");
+            //        Console.ReadLine();
+            //    }
+
+            //}
+            #endregion
+
+            #region Sprawdz czy podany numer PESEL jest poprawny
+            bool endLoop = true;
+            
+            while (endLoop) 
+            {
+                Console.WriteLine("Enter your PESEL number"); 
+                var userImput = Console.ReadLine();
+                if (userImput == "q")  // wielokrotna mozliwosc wpisywania numeru PESEL
+                {
+                    endLoop = false;
+                    continue;
+                }
+                string [] num = new num[11];
+
+                int checkinNum = 9a + 7b + 3c + 1d + 9e + 7f + 3g + 1h + 9i + 7j;
+                var result = int.TryParse(userImput, out checkinNum);
+               
+            
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ Zadania/Pyramid/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Description of the excertice
+            //Napisać metodę, która w zależności od podanego h narysuje na konsoli piramide.
+            //Proszę sczytać wartość h z konsoli.
+            //Np.
+            //dla h = 2
+            //*
+            //**
+            //Dla h = 3
+            //  *
+            //**
+            //***
+            //Dla h = 4
+            //   *
+            //  **
+            //***
+            //****
+            //Dla wartości ujemnych piramida powinna być rysowana do góry nogami
+
+            //string[] choinka = new string[4];
+            //string star = "*";
+            //choinka[0] = star;
+            //string slowo1 = "**";
+            //choinka[1] = slowo1;
+            //string slowo2 = "***";
+            //choinka[2] = slowo2;
+            //string slowo3 = "****";
+            //choinka[3] = slowo3;
+
+            #endregion
+
+            PyramidDraw();
+            Console.ReadLine();
+        }
+        public static void PyramidDraw() 
+        {
+            Console.WriteLine(" Enter a hight of a pyramid");
+            int h = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Choose a char for creating a pyramid");
+            char znak = char.Parse(Console.ReadLine());
+            Console.WriteLine("Normal(1) view or inverted (2)");
+            string imput = Console.ReadLine();
+
+            switch (imput.ToLower()) 
+            {
+                case "1"://normal
+                    Console.WriteLine(" Wybrałes normalną");
+                    {
+                        for (int rzad = 0; rzad < h; rzad++)
+                        {
+                            for (int przestrzen = h - rzad; przestrzen > rzad; przestrzen--)
+                            {
+                                Console.WriteLine(" ");
+                            }
+                            for (int kolumna = 0; kolumna < (2 * rzad + 1); kolumna++)
+                            {
+                                Console.WriteLine(znak);
+                            }
+                            Console.WriteLine();
+                        }
+                        Console.WriteLine("Zakonczono obliczanie");
+                        Console.ReadLine();
+                        break;
+
+                    }
+                case "2"://inverted
+                    Console.WriteLine("Odrócona konstrukcja");
+                    { 
+                        for(int rzad=h;rzad >0;rzad--) 
+                        {
+                            for (int przestrzen = rzad; przestrzen < h; przestrzen++)
+                            {
+                                Console.WriteLine(" ");
+                            }
+                            for (int j = (2 * rzad - 1); j > 0; j--)
+                            {
+                                Console.WriteLine(znak);
+                            }
+                            Console.WriteLine();
+
+                        }
+                        Console.WriteLine("Zakonczono obliczanie");
+                        Console.ReadLine();
+
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Wprowadz wartosci raz jeszcze");
+                    break;
+
+            }
+            Console.ReadLine();
+
+           
+
+            
+            
+        
+        }
+    }
+}
+
